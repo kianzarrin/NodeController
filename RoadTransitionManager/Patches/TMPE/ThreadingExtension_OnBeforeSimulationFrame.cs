c0eab63b7caf5c69bbb93a50e78df39570264656
kian.zarrin <kian.zarrin@gmail.com>
2020-05-08 17:07:52 +0300

    disable TMPE detour test.

---
+++ RoadTransitionManager/Patches/TMPE/ThreadingExtension_OnBeforeSimulationFrame.cs
@@ -0,0 +1,17 @@
+namespace RoadTransitionManager.Patches.TMPE {
+    using System.Reflection;
+    using HarmonyLib;
+    using TrafficManager;
+    using System;
+
+    [HarmonyPatch(typeof(ThreadingExtension), nameof(ThreadingExtension.OnBeforeSimulationFrame))]
+    public static class ThreadingExtension_OnBeforeSimulationFrame {
+        static FieldInfo field_firstFrame =
+            AccessTools.Field(typeof(ThreadingExtension), "firstFrame") ??
+            throw new Exception("could not find ThreadingExtension.firstFrame");
+        public static void Prefix(ThreadingExtension __instance) {
+            //Util.Log.Debug("ThreadingExtension_OnBeforeSimulationFrame.Prefix called");
+            field_firstFrame.SetValue(__instance, false);
+        }
+    }
+}
