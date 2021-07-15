61d4592ca539fab5228522907b311bd146a98a75
kianzzarrin <kian.zarrin@gmail.com>
2021-07-15 16:16:14 +0300

    doc

--- NodeController/Manager/SegmentEndDataProperty.cs
+++ NodeController/Manager/SegmentEndDataProperty.cs
@@ -14,6 +14,12 @@ namespace NodeController.Manager {
         }
         public static SegmentEndDataProperty<TName, TVal> GetAt(SegmentEndData segmentEndData)
             => fieldRef_(segmentEndData);
+
+        /// <summary>
+        /// I found no other way of overriding ShouldShow(NodeData)
+        /// </summary>
+        /// <param name="nodeData"></param>
+        /// <returns></returns>
         public static SegmentEndDataProperty<TName, TVal> FirstAt(NodeData nodeData)
             => fieldRef_(nodeData.IterateSegmentEndDatas().First());
 
@@ -23,9 +29,12 @@ namespace NodeController.Manager {
             }
         }
 
+        // tests if all segment ends attached to this nodes have the same value or not
+        // node panel dispalys the average value of non-unifrom items with a yellow highlight.
         public static bool IsUniform(NodeData nodeData) =>
             nodeData.IterateSegmentEndDatas().AllEqual(item => GetAt(item).Value);
 
+        // shortchuts:
         public static bool ShouldShow0(SegmentEndData segmentEndData) =>
             GetAt(segmentEndData).ShouldShow(segmentEndData);
         public static bool ShouldShow0(NodeData nodeData) =>
@@ -35,20 +44,30 @@ namespace NodeController.Manager {
         #endregion
         public TVal Value;
 
+        // useful when removing or reseting data
         public virtual TVal DefaultValue => default(TVal);
 
+        // should we show this toggle in segmentEnd panel?
         public abstract bool ShouldShow(SegmentEndData segmentEndData);
-        public abstract bool ShouldShow(NodeData nodeData);
+
+
+        // should we show this toggle in node panel?
+        public virtual bool ShouldShow(NodeData nodeData) =>
+            nodeData.IterateSegmentEndDatas().Any(item=> ShouldShow0(item));
+
+        // can this value be modified? if not it should be reset to DefaultValue
         public abstract bool CanModify(SegmentEndData segmentEndData);
     }
 
     public abstract class SegmentEndDataPropertyToggle<TName> : SegmentEndDataProperty<TName, bool> {
+        // node panel dispalys the average value of non-unifrom items with a yellow highlight.
         public static bool GetValue(NodeData nodeData) {
             return nodeData.IterateSegmentEndDatas().Any(item => GetAt(item).Value);
         }
     }
 
     public abstract class SegmentEndDataPropertyFloat<TName> : SegmentEndDataProperty<TName, float> {
+        // node panel dispalys the average value of non-unifrom items with a yellow highlight.
         public static float GetValue(NodeData nodeData) {
             return nodeData.IterateSegmentEndDatas().Average(item => GetAt(item).Value);
         }
