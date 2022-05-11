d750434eff718ae958a0650cd060910420bed2c1
kianzzarrin <kian.zarrin@gmail.com>
2022-05-12 02:07:23 +0300

    catch simulation error.

--- NodeController/Patches/NetSegmentPatches/CalculateSegment.cs
+++ NodeController/Patches/NetSegmentPatches/CalculateSegment.cs
@@ -1,16 +1,22 @@
 namespace NodeController.Patches {
     using HarmonyLib;
     using KianCommons;
+    using System;
+
     [HarmonyPatch(typeof(NetSegment), nameof(NetSegment.CalculateSegment))]
     class CalculateSegment {
         static void Postfix(ushort segmentID) {
-            if (!NetUtil.IsSegmentValid(segmentID)) return;
-            SegmentEndData segStart = SegmentEndManager.Instance.GetAt(segmentID, true);
-            SegmentEndData segEnd = SegmentEndManager.Instance.GetAt(segmentID, false);
-            segStart?.OnAfterCalculate();
-            segEnd?.OnAfterCalculate();
-            segStart?.NodeData.ShiftPilar();
-            segEnd?.NodeData.ShiftPilar();
+            try {
+                if (!NetUtil.IsSegmentValid(segmentID)) return;
+                SegmentEndData segStart = SegmentEndManager.Instance.GetAt(segmentID, true);
+                SegmentEndData segEnd = SegmentEndManager.Instance.GetAt(segmentID, false);
+                segStart?.OnAfterCalculate();
+                segEnd?.OnAfterCalculate();
+                segStart?.NodeData.ShiftPilar();
+                segEnd?.NodeData.ShiftPilar();
+             } catch(Exception ex) {
+                ex.Log($"segment:{segmentID}");
+            }
         }
     }
 }
